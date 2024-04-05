49873333c09b0c16ad9ad7a24614e4d018129428
dankank22 <ankithsworld@gmail.com>
2024-04-04 19:19:45 -0700

    new change

---
+++ viable-veganism/app/resources.js
@@ -0,0 +1,40 @@
+export default function resources (){
+    return (
+        <body>
+        <header className={`${styles.main} ${styles.header}`}>
+            <nav class="flex flex-row justify-between w-11/12">
+                <section class="flex flex-row items-center gap-3">
+                    <img src="vegan-logo.png" />
+                    <div className={styles["logo-txt"]}>
+                        <div >
+                            Viable
+                        </div>
+                        <div >
+                            Veganism
+                        </div>
+                    </div>
+                </section>
+
+                <section class="flex flex-row items-center 
+                                gap-14 text-lg">
+                    <Link href="/home" className={styles["home-button"]}>
+                        Home
+                    </Link>
+                    <Link href="/about">
+                        About us
+                    </Link>
+                    <Link href="/resources">
+                        Resources
+                    </Link>
+                    <Link href="/contact">
+                        Contact us
+                    </Link>
+                </section>
+            </nav>
+        </header>
+
+       <img className="giraffe" src={require("./images/pagepic.jpeg")} alt="resourcespagepic"></img>
+       
+       </body>
+  );
+}
