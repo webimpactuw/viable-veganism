9387c8d74a39c710adfabd9e08a8011599123cb4
williamtsai726 <williamtsai726@gmail.com>
2024-07-18 16:44:19 -0700

    open up analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./component/Header.js";
 import Footer from "./component/Foot.js";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default function RootLayout({ children }) {
       <Header />
       {children}
       <Footer />
+      <Analytics/>
       </body>
     </html>
   );
